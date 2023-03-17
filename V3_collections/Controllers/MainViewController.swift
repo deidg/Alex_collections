ef44ca19e4c49eac7c1fb17f6c804eb60adeff31
deidg <64797316+deidg@users.noreply.github.com>
2023-03-17 20:33:26 +0300

    commit 1.2.0 label attempt

--- V3_collections/Controllers/MainViewController.swift
+++ V3_collections/Controllers/MainViewController.swift
@@ -8,31 +8,52 @@ import UIKit
 import SnapKit
 
 class MainViewController: UIViewController {
-    let tableView = UITableView()
+//    private let tableView = UITableView()
+//    private let mainLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setupItemsOnView()
     }
     
-    let mainTableView: UITableView = {
+    // MARK: Items
+    private let mainTableView: UITableView = {
         let mainTableView = UITableView()
-        
-        mainTableView.backgroundColor = .systemGray
-        
-        
+        mainTableView.backgroundColor = .red // Constants.Colors.backroundColor
         return mainTableView
     }()
+
+//    private let mainLabel: UILabel = {
+//        let mainLabel = UILabel()
+//        mainLabel.backgroundColor = .orange // Constants.Colors.backroundColor
+//        return mainLabel
+//    }()
     
     // MARK: UI setup
     private func setupItemsOnView() {
+        
         view.addSubview(mainTableView)
-        mainTableView.snp.makeConstraints{ make in
-        make.edges.width.height.equalTo(view)
+        mainTableView.snp.makeConstraints { make in
+            make.edges.equalTo(view)
+        }
+        
+//                view.addSubview(mainLabel)
+//        mainLabel.snp.makeConstraints { make in
+//                    make.edges.equalTo(view)
+//                }
+        
+//        mainView.addSubview(mainLabel)
+//        mainLabel.snp.makeConstraints { make in
+//            make.top.equalTo(mainView).inset(10)
+//            make.height.equalTo(100)
+//        }
+    }
+}
+extension MainViewController {
+    // MARK: extension MainVC - constants
+    enum Constants {
+        enum Colors {
+            static let backroundColor = UIColor(red: 125, green: 125, blue: 125, alpha: 1.0)
         }
     }
-    
-    
-    
 }

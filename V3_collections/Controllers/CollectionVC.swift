6e6b60f15514b1a6c40c786ed1698205e27d9180
deidg <64797316+deidg@users.noreply.github.com>
2023-03-18 16:08:55 +0300

    commit 1.3 navigationController done

---
+++ V3_collections/Controllers/CollectionVC.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionVC.swift
+//  V3_collections
+//
+//  Created by Alex on 18.03.2023.
+//
+
+import Foundation
+import UIKit
+
+class CollectionVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view?.backgroundColor = .systemPink
+    }
+}

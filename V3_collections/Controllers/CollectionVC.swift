761da9ad44167603d4312f75aa48c804ec585c07
deidg <64797316+deidg@users.noreply.github.com>
2023-03-19 09:43:47 +0300

    commit 1.3.4

--- V3_collections/Controllers/CollectionVC.swift
+++ V3_collections/Controllers/CollectionVC.swift
@@ -13,7 +13,7 @@ class CollectionVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavBar()
-        view?.backgroundColor = .systemPink
+        view.backgroundColor = .systemRed
     }
     
     func setupNavBar() {
@@ -22,6 +22,45 @@ class CollectionVC: UIViewController {
         navigationController?.navigationBar.backgroundColor = .yellow
     }
     
-
+    @objc func didTapButton() {
+        navigationController?.pushViewController(ArrayVC(), animated: true)
+    }
+    
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    
+    
+    
 }
 
+
+class ArrayVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+}
+
+extension MainViewController: UITableViewDelegate{
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath:  IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "String")
+//
+//    return cell
+//    }
+    
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return CGFloat
+//    })
+}

574d09e1d0ebe4efac8899bd87d3f9a1df2a37c1
deidg <64797316+deidg@users.noreply.github.com>
2023-03-22 14:27:06 +0300

    commit 1.3.8.6

--- V3_collections/Controllers/CollectionVC.swift
+++ V3_collections/Controllers/CollectionVC.swift
@@ -9,46 +9,25 @@ import Foundation
 import UIKit
 
 class CollectionVC: UIViewController {
-    //    let arrayVC: [UIViewController] = [arrayController, setController, dictionaryController]
-    //        var array2 = [UIViewController]()
-    
+
     let arrayController = ArrayController()
     let setController = SetController()
     let dictionaryController = DictionaryController()
     
-    let arrayVC: [UIViewController] = []()
-    arrayVC.append(arrayController)
-    arrayVC.append(setController)
-    arrayVC.append(dictionaryController)
-    
-    
+    let arrayVC = ["Array", "Set", "Dictionary"]
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         //        setup()
         setupNavBar()
         view.backgroundColor = .gray
         setupViews()
-        
-        //        let initialPage = 0
-        
-        //
-        //                        array2.append(arrayController)
-        //                        array2.append(setController)
-        //                        array2.append(dictionaryController)
     }
     
     let cellId = "cellId"
     
-    //        var tableView = UITableView()
     let tableView = UITableView.init(frame: .zero, style: UITableView.Style.grouped)  // framed вместо grouped
-    //    self.tableView.register(TableViewCell.self, forCellReuseIdentifier: "TableViewCell")
-    
-    //    func setup() {
-    //        let arrayController = ArrayController()
-    //        let setController = SetController()
-    //        let dictionaryController = DictionaryController()
-    //    }
-    
+  
     func setupViews() {
         tableView.delegate = self
         tableView.dataSource = self
@@ -64,34 +43,17 @@ class CollectionVC: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.backgroundColor = .yellow
     }
-    
-    
-    
 }
 
 class TableViewCell: UITableViewCell {
     
 }
 
-
 // MARK: extensions
 extension CollectionVC: UITableViewDelegate {
     
-    //        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    //            return arrayVC.count
-    //        }
-    //        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    //            var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
-    //            if cell == nil {
-    //                cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-    //            }
-    //            cell?.textLabel?.text = arrayVC[indexPath.row]
-    //            return cell!
-    //        }
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        navigationController?.pushViewController(arrayVC[indexPath.row], animated: true)   // .viewController, animated: true)
-        //    }
+        present(arrayVC[indexPath.row], animated: true)
     }
 }
 extension CollectionVC: UITableViewDataSource {
@@ -105,8 +67,7 @@ extension CollectionVC: UITableViewDataSource {
         if cell == nil {
             cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
         }
-        cell?.textLabel?.text = arrayVC[indexPath.row]
+        cell?.textLabel?.text = String(arrayVC[indexPath.row])
         return cell!
     }
 }
-

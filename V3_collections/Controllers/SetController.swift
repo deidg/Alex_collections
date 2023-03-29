cf2294a9939b248f9146081532df8ce60ca07e27
deidg <64797316+deidg@users.noreply.github.com>
2023-03-29 09:22:31 +0300

    1.3.14.2 iOAcademy version step 2 -Compositional Layout simple (large pictures) 2 by 2

--- V3_collections/Controllers/SetController.swift
+++ V3_collections/Controllers/SetController.swift
@@ -15,7 +15,7 @@ class SetController: UIViewController {
     
     private let collectionView = UICollectionView(
         frame: .zero,
-        collectionViewLayout: UICollectionViewFlowLayout()
+        collectionViewLayout: SetController.createLayout()
     )
     
     override func viewDidLoad() {
@@ -26,11 +26,37 @@ class SetController: UIViewController {
         collectionView.backgroundColor = .white
         collectionView.dataSource = self
     }
+    
+    static func createLayout() -> UICollectionViewCompositionalLayout {
+        // Item
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalHeight(1)
+            )
+        )
+        // Group
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalHeight(2/5)
+            ),
+            subitem: item,
+            count: 2
+        )
+        
+        // Sections
+        let section = NSCollectionLayoutSection(group: group)
+        
+        
+        // Return
+        return UICollectionViewCompositionalLayout(section: section)
+    }
 }
 
 
 extension SetController: UICollectionViewDataSource {
-   
+    
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 30

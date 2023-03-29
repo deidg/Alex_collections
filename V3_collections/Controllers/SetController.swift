624c8333f70d0491ac4d402cc24d7383f608996d
deidg <64797316+deidg@users.noreply.github.com>
2023-03-29 08:48:22 +0300

    1.3.14.1 iOAcademy version  step 1 - collectionView Standard

--- V3_collections/Controllers/SetController.swift
+++ V3_collections/Controllers/SetController.swift
@@ -15,16 +15,32 @@ class SetController: UIViewController {
     
     private let collectionView = UICollectionView(
         frame: .zero,
-        collectionViewLayout: UICollectionViewLayout()
+        collectionViewLayout: UICollectionViewFlowLayout()
     )
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(collectionView)
         collectionView.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
-        
+        collectionView.frame = view.bounds
+        collectionView.backgroundColor = .white
+        collectionView.dataSource = self
+    }
+}
+
+
+extension SetController: UICollectionViewDataSource {
+   
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
     }
     
-    
-    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.identifier, for: indexPath)
+        
+        return cell
+    }
 }
+
+

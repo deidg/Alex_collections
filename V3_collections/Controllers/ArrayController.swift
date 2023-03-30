34cbed021239c4954f91779b6174ff8306a20308
deidg <64797316+deidg@users.noreply.github.com>
2023-03-30 18:42:12 +0300

    commit 1.3.15.2 migration done. before adding activity indicator

--- V3_collections/Controllers/ArrayController.swift
+++ V3_collections/Controllers/ArrayController.swift
@@ -1,198 +1,89 @@
 //
-//  ArrayNavigationController.swift
+//  SetNavigationController.swift
 //  V3_collections
 //
-//  Created by Alex on 19.03.2023.
+//  Created by Alex on 21.03.2023.
 //
 
-
-//iOs academy - https://www.youtube.com/watch?v=vAhas_my5mo&t=45s
-// разобраться как делить на секции и строчки.
-
-
-import Foundation
 import UIKit
 import SnapKit
 
 class ArrayController: UIViewController {
     
-    private let arrayCollectionView: UICollectionView = {
-        let arrayCollectionViewLayout = UICollectionViewLayout()
-        let arrayCollectionView = UICollectionView(frame: .zero, collectionViewLayout: arrayCollectionViewLayout)
-        arrayCollectionView.backgroundColor = .none
-        arrayCollectionView.bounces = false
-        arrayCollectionView.translatesAutoresizingMaskIntoConstraints = false
-        return arrayCollectionView
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        //        layout.minimumInteritemSpacing = 0
+        //        layout.minimumLineSpacing = 0
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .green
+        
+        return collectionView
     }()
-    
-//    private let sections = MockData.shared.pageData
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view?.backgroundColor = .systemBackground
-        setupNavBar()
-        setupViews()
+        setupContraints()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: "MyCollectionViewCell")
     }
     
-    func setupViews() {
-        view.backgroundColor = .white
-        view.addSubview(arrayCollectionView)
-        
-        arrayCollectionView.register(ArrayCollectionViewCell.self, forCellWithReuseIdentifier: ArrayCollectionViewCell.identifier)
-        arrayCollectionView.register(ArrayCVTilesCell.self, forCellWithReuseIdentifier: ArrayCollectionViewCell.identifier)
-        
-        
-        
-        //        tableViewArray = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
-        //        tableViewArray.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        //        tableViewArray.backgroundColor = .gray
-        //
-        //        //  еще раз найти откуда появляется эта ячейка "cell". не аттавизм ли это?
-        ////        tableViewArray.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "ArrayCollectionViewCell")
-        //
-        
-        //
-        //        //1.33.20
-        //        setupDataSource()
-        //        reloadData()
-        //
-        ////        tableViewArray.delegate = self
-        ////        tableViewArray.dataSource = self
-            }
-        
-        private func setDelegates() {
-//            arrayCollectionView.delegate = self
-//            arrayCollectionView.dataSource = self
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.row == 0 {
+            return CGSize(width: collectionView.bounds.width, height: 100)
+        } else {
+                        return CGSize(width: (collectionView.bounds.width/2), height: 100)   //
+            
+//            let flowayout = collectionViewLayout as? UICollectionViewFlowLayout
+//            let space: CGFloat = (flowayout?.minimumInteritemSpacing ?? 0.0) +
+//            (flowayout?.sectionInset.left ?? 0.0) +
+//            (flowayout?.sectionInset.right ?? 0.0)
+//            let size: CGFloat = (collectionView.frame.size.width - space) / 2.0
+//            return CGSize(width: size, height: 100)
         }
-        //}
-        
-//        func configure<T: SelfConfiguringCell>(cellType: T.Type, with intValue: Int, for indexPath: IndexPath) -> T {
-//            guard let cell =  tableViewArray.dequeueReusableCell(withReuseIdentifier: cellType.reuseId, for: indexPath) as? T else {
-//                fatalError("Error \(cellType)")
-//            }
-//            return cell
-//        }
-//
-//        private func setupDataSource() {
-//            dataSource = UICollectionViewDiffableDataSource<SectionKind, Int>(collectionView: tableViewArray, cellProvider: { (tableViewArray, indexPath, intValue) -> UICollectionViewCell? in
-//                let section = SectionKind(rawValue: indexPath.section)!  //  ПРОВЕРИТЬ что будет если убрать знак !
-//                switch section {
-//                default:
-//                    return self.configure(cellType: ArrayCollectionViewCell.self, with: intValue, for: indexPath)
-//                }
-//            })
-//        }
-
-        //    func reloadData() {
-        //        var snapshot = NSDiffableDataSourceSnapshot<SectionKind, Int>()
-        //        //        let itemPerSection = 10
-        //        SectionKind.allCases.forEach { (sectionKind) in
-        //            snapshot.appendSections([sectionKind])
-        //            snapshot.appendItems([1], toSection:  sectionKind)
-        //        }
-        //        dataSource.apply(snapshot, animatingDifferences: false)
-        //    }
-        
-        private func createLayout() -> UICollectionViewLayout {
-            // section -> group -> items -> size
-            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                  heightDimension: .fractionalHeight(1.0))
-            let item = NSCollectionLayoutItem(layoutSize: itemSize)
-            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                   heightDimension: .fractionalHeight(0.2)) // вот тут он в 49.00 сделал .fractioalWidth
-            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-            let section = NSCollectionLayoutSection(group: group)
-            let layout = UICollectionViewCompositionalLayout(section: section)
-            return layout
-        }
-//    }
-        
-        func setupNavBar() {
-            navigationController?.navigationBar.topItem?.title = "Dictionary"
-            navigationController?.navigationBar.prefersLargeTitles = false
-            navigationController?.navigationBar.backgroundColor = .yellow
-        }
-        
-//    }
-}
-
-// MARK:  extensions
-extension ArrayController: UICollectionViewDelegate {
-    
-}
-
-//extension ArrayController: UICollectionViewDataSource {
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        <#code#>
-//    }
-    
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-//        sections.count
-        return 1
     }
     
+    func setupContraints() {
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints{ make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+extension ArrayController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        sections[section].count
-        return 1
+        return 26
     }
     
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        switch sections[indexPath.section] {
-//
-//        case .firstRow(let firstRow):
-//            guard let cell = arrayCollectionView.dequeueReusableCell(withReuseIdentifier: arrayCollectionViewCell,
-//                                                                     for: indexPath) as? ArrayCollectionViewCell
-//            else {
-//                return UICollectionViewCell()
-//            }
-//            cell.configureCell(imageName: firstRow[indexPath.row].image)//  Название картинки
-//            return cell
-//
-//        case.tiles(let tiles):
-//            guard let cell = arrayCollectionView.dequeueReusableCell(withReuseIdentifier: arrayCollectionViewCell,
-//                                                                     for: indexPath) as? ArrayCollectionViewCell
-//            else {
-//                return UICollectionViewCell()
-//            }
-//            cell.configureCell(imageName: tiles[indexPath.row].image)  // d 16.40 он говорит о тайтле
-//            return cell
-//        }
-//    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCollectionViewCell",
+                                                            for: indexPath) as? MyCollectionViewCell else
+        { return UICollectionViewCell() }
+        cell.backgroundColor = .yellow
+        return cell
+    }
     
-//    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String,
-//                        at indexPath: IndexPath) -> UICollectionReusableView {
-//
-////        switch kind {
-////        case arrayCollectionView.elementKindSectionHeader:
-////            let header = arrayCollectionView.dequeueReusableSupplementaryView(ofKind: kind,
-////                                                                              withReuseIdentifier: "header",
-////                                                                              for: indexPath) as! HeaderSupplementaryView
-////            header.configureHeader
-////
-////            return header
-////        default:
-////            return UICollectionReusableView()
-////        }
-//    }
-//}
+}
 
-//extension ArrayController {
-//
-//    private func setConstraints() {
-//
-//        NSLayoutConstraint.activate([
-//            arrayCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
-//            arrayCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
-//            arrayCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
-//            arrayCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
-//        ])
-//
-//    }
-//}
-    
-    
+extension ArrayController: UICollectionViewDelegate {
+}
+
+extension ArrayController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
 
 
 

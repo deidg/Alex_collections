b55f3d15b8bb611681eb71369104757f94dc634a
deidg <64797316+deidg@users.noreply.github.com>
2023-03-31 18:16:13 +0300

    commit 1.3.16.2 image with label done MVP

--- V3_collections/Model/MyCollectionViewCell.swift
+++ V3_collections/Model/MyCollectionViewCell.swift
@@ -12,39 +12,22 @@ import SnapKit
 
 class MyCollectionViewCell: UICollectionViewCell {
     static var identifier = "MyCollectionViewCell"
-//   let selectedImagae = UIImage = "pic1"
-//    let imageView = UIImageView()
-   
-//    let label: UILabel = {
-//        let label = UILabel()
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//        label.text = "Create Int array with"
-//        label.font = .systemFont(ofSize: 20, weight: .bold)
-//        return label
-//    }()
+    //   let selectedImagae = UIImage = "pic1"
+    //    let imageView = UIImageView()
     
+        let label: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.text = "Create Int array with"
+//            label.backgroundColor = .
+            label.font = .systemFont(ofSize: 20, weight: .bold)
+            return label
+        }()
     
-    //========================
-    private let imageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.clipsToBounds = true
-        imageView.contentMode = .scaleAspectFill
-//        imageView.frame.height = 30
-        return imageView
-    }()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        contentView.addSubview(imageView)
-        
-        let images: [UIImage] = [
-            UIImage(named: "pic1"),
-//            UIImage(named: "pic2"),
-//            UIImage(named: "pic3"),
-        ].compactMap({ $0 })
-        imageView.image = images.randomElement()
-        contentView.clipsToBounds = true
-    }
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            contentView.addSubview(label)
+}
     
     required init?(coder: NSCoder) {
         fatalError()
@@ -52,7 +35,40 @@ class MyCollectionViewCell: UICollectionViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        imageView.frame = contentView.bounds
+        label.frame = contentView.bounds
     }
 }
+    
+    
+    //========================
+//    private let imageView: UIImageView = {
+//        let imageView = UIImageView()
+////        imageView.clipsToBounds = true
+////        imageView.contentMode = .scaleAspectFill
+////        imageView.frame.height = 30
+//        return imageView
+//    }()
+    
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        contentView.addSubview(imageView)
+        
+//        let images: [UIImage] = [
+//            UIImage(named: "pic1"),
+////            UIImage(named: "pic2"),
+////            UIImage(named: "pic3"),
+//        ].compactMap({ $0 })
+//        imageView.image = images.randomElement()
+//        contentView.clipsToBounds = true
+//    }
+    
+//    required init?(coder: NSCoder) {
+//        fatalError()
+//    }
+    
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//        imageView.frame = contentView.bounds
+//    }
+//}
 

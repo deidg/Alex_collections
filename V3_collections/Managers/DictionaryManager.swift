da45b860044fb04507870225db2770415a293b76
deidg <64797316+deidg@users.noreply.github.com>
2023-05-04 10:19:50 +0300

    commit 1.4.15.3 names and random phone numbers done

--- V3_collections/Managers/DictionaryManager.swift
+++ V3_collections/Managers/DictionaryManager.swift
@@ -22,12 +22,14 @@ class DictionaryManager {
     init() {
         for i in 0..<10 {
             let name = "name\(i)"
-            let phoneNumber = "333-333-\(i)"
+            var randomNumber = Int.random(in: 1111111...9999999)
+            let phoneNumber = String(randomNumber)
             let contact = Contact(name: name, phoneNumber: phoneNumber)
             
             contactArr.append(contact)
             
                     print(contactArr[i].name)
+            print(contactArr[i].phoneNumber)
             //        i += 1
         }
     }
